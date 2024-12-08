590d56cc1172036f51992126f3a80428aefbdbae
ebrahim <ebrahimmosadegh@gmail.com>
2024-12-08 19:59:33 +0330

    create navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Hero from "./components/Hero"
 import About from "./components/About"
+import Navbar from "./components/Navbar"
 function App() {
   return (
-    <main className="relative min-h-screen w-screen overflow-x-hidden">
+    <main className="relative min-h-screen w-screen overflow-x-hidden bg-zinc-600">
+      <Navbar />
       <Hero />
       <About />
     </main>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -59,7 +59,7 @@ const Hero = () => {
 
     useGSAP(() => {
         gsap.set('#video-frame', {
-            clipPath: 'polygon(14% 0%, 72% 0%, 90% 90%, 0% 100%)',
+            clipPath: 'polygon(0 0, 100% 0%, 100% 70%, 52% 100%, 0 70%)',
             borderRadius: '0 0 40% 10%'
         })
 
@@ -123,25 +123,25 @@ const Hero = () => {
               />
             </div>
             <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">
-                G<b>a</b>ming
+                Sp<b>a</b>ce
             </h1>
             <div className="absolute left-0 top-0 z-40 size-full">
                 <div className="mt-24 px-5 sm:px-10">
-                    <h1 className="special-font hero-heading text-blue-100">redefi<b>n</b>e</h1>
+                    <h1 className="special-font hero-heading text-blue-100">St<b>a</b>rs</h1>
                     <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
-                        Enter the Metagame Layer <br /> Unleash the play Economy
+                    What is Space? <br /> Space is a vast, seemingly infinite expanse that exists beyond the Earth's atmosphere.
                     </p>
                     <Button 
                     id="watch-trailer" 
-                    title="Watch Trailer" 
+                    title="Watch Video" 
                     leftIcon={<TiLocationArrow />}
-                    containerClass="!bg-yellow-300 flex-center gap-1"
+                    containerClass="!bg-cyan-500 flex-center gap-1"
                     />
                 </div>
             </div>
         </div>
         <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
-                G<b>a</b>ming
+            Sp<b>a</b>ce
         </h1>
     </div>
   )
