6c122722853cf88e1426f02d1e749bb9c0f5568c
ebrahim <ebrahimmosadegh@gmail.com>
2024-12-18 18:48:01 +0330

    update media

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,15 +16,15 @@ const Contact = () => {
                 src='img/contact-1.webp'
             />
             <ImageClipBox 
-                clipClass='contact-clip-path-2 lg:translate-y-40 translate-y-60'
+                clipClass='contact-clip-path-2 lg:-translate-y-10'
                 src='img/contact-2.webp'
             />
         </div>
         <div className='absolute -top-40 left-20 w-60 sm:top-1/2 md:left-auto md:right-10 lg:top-20 lg:w-80'>
-            <ImageClipBox 
+            {/* <ImageClipBox 
                 src='img/swordman-partial.webp'
                 clipClass='absolute md:scale-125'
-            />
+            /> */}
             <ImageClipBox 
                 src='img/swordman.webp'
                 clipClass='sword-man-clip-path md:scale-125'
